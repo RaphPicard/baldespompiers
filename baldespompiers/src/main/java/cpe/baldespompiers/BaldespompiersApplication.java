8d57bdc188d86cd1bd14f260f45ab0450b421d41
evanbnm <evan.benhamou@icloud.com>
2026-05-11 09:14:20 +0200

    Initial commit

---
+++ baldespompiers/src/main/java/cpe/baldespompiers/BaldespompiersApplication.java
@@ -0,0 +1,13 @@
+package cpe.baldespompiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaldespompiersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaldespompiersApplication.class, args);
+	}
+
+}
---
+++ baldespompiers/src/test/java/cpe/baldespompiers/BaldespompiersApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.baldespompiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaldespompiersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
